3973d1e17a6f1e1a3ace18cee669cae2a8f48873
zd-prog <darya.ziziko@mail.ru>
2022-09-11 16:10:34 +0300

    User and Restaurant forms were created

--- coreapp/views.py
+++ coreapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
+from coreapp.forms import UserForm, RestaurantForm
+
 # Create your views here.
 def home(request):
   return redirect(restaurant_home)
@@ -10,4 +12,10 @@ def restaurant_home(request):
   return render(request, "restaurant/home.html", {})
 
 def restaurant_sign_up(request):
-  return render(request, "restaurant/sign_up.html", {})
+  user_form = UserForm()
+  restaurant_form = RestaurantForm()
+
+  return render(request, "restaurant/sign_up.html", {
+    "user_form": user_form,
+    "restaurant_form": restaurant_form
+  })
