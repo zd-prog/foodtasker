e422a566b8bbea0e106334b6ab1540dfd94f2e01
zd-prog <darya.ziziko@mail.ru>
2022-09-11 14:55:46 +0300

    Home page was created

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import imp
 from django.contrib import admin
 from django.urls import path
+from coreapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+
 ]
