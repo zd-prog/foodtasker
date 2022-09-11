e7f48b8c710906d07c4eb231b38033fd4c8cecba
zd-prog <darya.ziziko@mail.ru>
2022-09-11 16:01:33 +0300

    Sign Up Form

--- coreapp/views.py
+++ coreapp/views.py
@@ -8,3 +8,6 @@ def home(request):
 @login_required(login_url='/restaurant/sign_in/')
 def restaurant_home(request):
   return render(request, "restaurant/home.html", {})
+
+def restaurant_sign_up(request):
+  return render(request, "restaurant/sign_up.html", {})
