7336248964a2eb52037cbf41b39946b8b9131204
zd-prog <darya.ziziko@mail.ru>
2022-09-12 10:41:05 +0300

    make it better

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
 
     path('restaurant/sign_in/', auth_views.LoginView.as_view(template_name='restaurant/sign_in.html'), name='restaurant_sign_in'),
     path('restaurant/sign_out/', auth_views.LogoutView.as_view(next_page='/'), name='restaurant_sign_out'),
-    path('restaurant/sign_up', views.restaurant_sign_up, name='restaurant_ышпт_гз'),
+    path('restaurant/sign_up', views.restaurant_sign_up, name='restaurant_sign_up'),
     path('restaurant/', views.restaurant_home, name='restaurant_home'),
 
 ]
