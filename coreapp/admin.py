d6014eab83999bd20c2afbe4a23a556196fe32cd
zd-prog <darya.ziziko@mail.ru>
2022-10-02 16:19:25 +0300

    Order and OrderDetails models

--- coreapp/admin.py
+++ coreapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from coreapp.models import Restaurant, Customer, Driver, Meal
+from coreapp.models import Restaurant, Customer, Driver, Meal, Order, OrderDetails
 # Register your models here.
 admin.site.register(Restaurant)
 admin.site.register(Customer)
 admin.site.register(Driver)
 admin.site.register(Meal)
+admin.site.register(Order)
+admin.site.register(OrderDetails)
--- coreapp/models.py
+++ coreapp/models.py
@@ -1,9 +1,13 @@
 from email.policy import default
 import imp
+from lib2to3.pgen2 import driver
+from random import choices
+from time import time
 from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.utils import timezone
 
 # Create your models here.
 class Restaurant(models.Model):
@@ -44,3 +48,37 @@ class Meal(models.Model):
 
   def __str__(self):
     return self.name
+
+class Order(models.Model):
+  COOKING = 1
+  READY = 2
+  ONTHEWAY = 3
+  DELIVERED = 4
+
+  STATUS_CHOISES = (
+    (COOKING, "Cooking"),
+    (READY, "Ready"),
+    (ONTHEWAY, "On the way"),
+    (DELIVERED, "Delivered"),
+  )
+
+  customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+  restaurant = models.ForeignKey(Restaurant, on_delete=models.PROTECT)
+  driver = models.ForeignKey(Driver, models.SET_NULL, blank=True, null=True)
+  address = models.CharField(max_length=500)
+  total = models.IntegerField()
+  status = models.IntegerField(choices=STATUS_CHOISES)
+  created_at = models.DateTimeField(default=timezone.now)
+  picked_at = models.DateTimeField(blank=True, null=True)
+
+  def __str__(self):
+    return str(self.id)
+
+class OrderDetails(models.Model):
+  order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name='order_details')
+  meal = models.ForeignKey(Meal, on_delete=models.PROTECT)
+  quantity = models.IntegerField()
+  sub_total = models.IntegerField()
+
+  def __str__(self):
+    return str(self.id)
