e2cf0bc8d8fc78f745a14da40729e392dd55cf6f
zd-prog <darya.ziziko@mail.ru>
2022-10-02 16:42:40 +0300

    Status changer

--- coreapp/views.py
+++ coreapp/views.py
@@ -101,6 +101,13 @@ def restaurant_edit_meal(request, meal_id):
 
 @login_required(login_url='/restaurant/sign_in/')
 def restaurant_order(request):
+  if request.method == "POST":
+    order = Order.objects.get(id=request.POST["id"])
+
+    if order.status == Order.COOKING:
+      order.status = Order.READY
+      order.save()
+      
   orders = Order.objects.filter(restaurant = request.user.restaurant).order_by("-id")
   return render(request, 'restaurant/order.html', {
     "orders": orders
