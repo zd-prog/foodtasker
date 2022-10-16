47e6d87afc46ed768b07d139fe01508591ff2538
zd-prog <darya.ziziko@mail.ru>
2022-10-16 14:24:46 +0300

    ready orders

--- coreapp/apis.py
+++ coreapp/apis.py
@@ -155,7 +155,14 @@ def customer_get_latest_order_status(request):
 # ========
 
 def driver_get_ready_orders(request):
-  return JsonResponse({})
+  orders = OrderSerializer(
+    Order.objects.filter(status=Order.READY, driver=None).order_by("-id"),
+    many=True
+  ).data
+
+  return JsonResponse({
+    "orders": orders
+  })
 
 def driver_get_pick_order(request):
   return JsonResponse({})
