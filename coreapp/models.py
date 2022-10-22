0b6aa99ab78732193fc95f5bbe8198f4b4427b77
zd-prog <darya.ziziko@mail.ru>
2022-10-22 19:47:44 +0300

    restaurant name

--- coreapp/models.py
+++ coreapp/models.py
@@ -18,7 +18,7 @@ class Restaurant(models.Model):
   logo = CloudinaryField('image')
 
   def __str__(self):
-    return self.names
+    return self.name
 
 class Customer(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='customer')
